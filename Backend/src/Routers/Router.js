09ed9dfc0290fc95a74dfaee579881de05ea7822
unknown <kawanwagnner7@gmail.com>
2024-06-06 15:31:59 -0300

    Delete old backend

--- Backend/src/Routers/Router.js
+++
@@ -1,9 +0,0 @@
-// routes/userRoutes.js
-
-const express = require("express");
-const router = express.Router();
-const userController = require("../Controllers/userController");
-
-router.post("/api/signUp", userController.createUser);
-
-module.exports = router;
