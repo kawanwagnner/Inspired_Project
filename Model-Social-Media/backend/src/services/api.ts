3902cf06562c8293445366f998f096d07e524a42
Kawan Wagnner <kawanwagnner7@gmail.com>
2024-06-17 15:58:48 -0300

    commit

--- Model-Social-Media/backend/src/services/api.ts
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export const axiosServer = axios.create({
-  baseURL: process.env.SERVER_URL
-});
--- Model-Social-Media/frontend/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  images: {
-    domains: ['lh3.googleusercontent.com', `${process.env.NEXT_PUBLIC_IMAGE_DOMAIN}`],
-  },
-}
-
-module.exports = nextConfig
